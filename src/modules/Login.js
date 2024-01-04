9ae340a8682508c98200774cb064201c3043b9fa
Silas Silva <125811185+devsilasfreitas@users.noreply.github.com>
2024-01-04 13:34:15 -0300

    Update Login.js

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -66,7 +66,7 @@ export default function Login () {
     })
     inputName.addEventListener('mouseover', function () {
         containerName.append(editName)
-        toogletheme()
+        toogleTheme()
     })
     inputName.addEventListener('mouseout', function() {
         if (!(document.activeElement === inputName)) {
@@ -75,7 +75,7 @@ export default function Login () {
     })
     inputName.addEventListener('focusin', function() {
         containerName.append(editName)
-        toogletheme()
+        toogleTheme()
     })
 
     const addTask = document.createElement('button')
