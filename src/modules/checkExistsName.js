bd6daebc60129a00d8247c0cc41dab94453bbb08
Silas Silva <125811185+devsilasfreitas@users.noreply.github.com>
2024-01-05 11:17:19 -0300

    Update checkExistsName.js

--- src/modules/checkExistsName.js
+++ src/modules/checkExistsName.js
@@ -1,9 +1,7 @@
 import Login from './Login.js';
 import Sign from './Sign.js';
-import toogleTheme from './toogleTheme.js';
 
 export default function checkExistsName () {
-    toogleTheme()
     if (localStorage.getItem('name')) {
         Login ()
     } else {
